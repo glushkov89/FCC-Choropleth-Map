b010a9c2f2a274b155fd5e53e0b1058956fcc038
glushkov89 <kellerman117@gmail.com>
2018-08-29 14:21:40 +0300

    Finished.

--- script/app.js
+++ script/app.js
@@ -89,10 +89,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			/*----------Map------------*/
 
 			const findData = (id) => {
-				let tmp = {
-					// bachelorsOrHigher: "N/A",
-					// fips: "N/A"
-				};
+				let tmp = {};
 				data.forEach((obj) => {
 					if (obj.fips === id) tmp = { ...obj };
 				});
@@ -115,10 +112,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				.append("path")
 				.attr("d", path)
 				.attr("class", "county")
-				.attr("fill", (d) => {
-					let tmp = findData(d.id).bachelorsOrHigher;
-					return tmp === "N/A" ? "red" : color(tmp);
-				})
+				.attr("fill", (d) => color(findData(d.id).bachelorsOrHigher))
 				.style("stroke", "black")
 				.style("stroke-width", "0.2")
 				.attr("data-fips", (d) => findData(d.id).fips)
@@ -134,7 +128,6 @@ document.addEventListener("DOMContentLoaded", function() {
 						.transition()
 						.duration(100)
 						.style("opacity", 0.9);
-					//.attr("data-education", () => "Hello World");
 				})
 				.on("mouseout", () => {
 					tooltipDiv
@@ -177,13 +170,15 @@ document.addEventListener("DOMContentLoaded", function() {
 			const lgAxis = d3
 				.axisBottom(lgSc)
 				.tickFormat(d3.format(".1f"))
-				.tickValues(tickVals);
+				.tickValues(tickVals)
+				.tickFormat((n) => n.toFixed(1) + "%");
+			//.tickFormat((n) => Math.round((n + 0.00001) * 100) / 100 + "%");
 			legend
 				.append("g")
 				.call(lgAxis)
 				.attr("id", "lg-axis")
 				.attr("transform", `translate(0,${lg.h})`);
-			//Centering legend in lower margin
+			//Positioning legend
 			legend.attr(
 				"transform",
 				`translate(${mg.l +
@@ -201,7 +196,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				.html(
 					"Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)"
 				);
-			//Centering title and description in data and top margin
+			//Centering title and description in top margin
 			title
 				.attr(
 					"x",
@@ -221,221 +216,4 @@ document.addEventListener("DOMContentLoaded", function() {
 				);
 		});
 	});
-
-	// console.log("US County Data");
-	// console.log(geo);
-	// console.log("US Education Data");
-	// console.log(data);
-
-	// const req = new XMLHttpRequest();
-	// req.open(
-	// 	"GET",
-	// 	"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json",
-	// 	true
-	// );
-	// req.send();
-	// req.onload = function() {
-	// 	const json = JSON.parse(req.responseText);
-	// 	const data = json.monthlyVariance;
-	// 	/*----------------D3 Code here-----------------*/
-	// 	//Margins
-	// 	const mg = { t: 70, b: 90, r: 30, l: 70 };
-	// 	//Bar
-	// 	const b = { h: 30, w: 3 };
-	// 	//Diag
-	// 	const d = {
-	// 		h: 12 * b.h,
-	// 		w: Math.ceil((b.w * data.length) / 12)
-	// 	};
-	// 	//SVG
-	// 	const s = {
-	// 		h: mg.t + d.h + mg.b,
-	// 		w: mg.l + d.w + mg.r,
-	// 		i: "svg-canvas"
-	// 	};
-
-	// 	//Legend
-	// 	const lg = { h: 15, w: 30 };
-	// 	//Month names for y-axis
-	// 	const mon = [
-	// 		"January",
-	// 		"February",
-	// 		"March",
-	// 		"April",
-	// 		"May",
-	// 		"June",
-	// 		"July",
-	// 		"August",
-	// 		"September",
-	// 		"October",
-	// 		"November",
-	// 		"December"
-	// 	];
-	// 	/*-----------Scales----------*/
-	// 	const x = d3
-	// 		.scaleBand()
-	// 		.domain(
-	// 			data.reduce((acc, obj) => {
-	// 				acc.includes(obj.year) ? null : acc.push(obj.year);
-	// 				return acc;
-	// 			}, [])
-	// 		)
-	// 		.range([0, d.w]);
-	// 	const y = d3
-	// 		.scaleBand()
-	// 		.domain(d3.range(1, 13))
-	// 		.range([0, d.h]);
-	// 	const color = d3
-	// 		.scaleQuantize()
-	// 		.domain([
-	// 			d3.min(data, (d) => d.variance + json.baseTemperature),
-	// 			d3.max(data, (d) => d.variance + json.baseTemperature)
-	// 		])
-	// 		.range(clr.reverse());
-	// 	/*------------SVG + Diagram------------*/
-	// 	const svg = d3
-	// 		.select("main")
-	// 		.append("svg")
-	// 		.attr("id", s.i)
-	// 		.attr("height", s.h)
-	// 		.attr("width", s.w);
-	// 	const diag = svg
-	// 		.append("g")
-	// 		.attr("height", d.h)
-	// 		.attr("width", d.w)
-	// 		.attr("transform", `translate(${mg.l},${mg.t})`);
-	// 	/*-------------Axis----------------*/
-	// 	const yAxis = d3.axisLeft(y).tickFormat((n, i) => mon[i]);
-	// 	const xAxis = d3
-	// 		.axisBottom(x)
-	// 		.tickValues(x.domain().filter((d, i) => !(i % 10)));
-	// 	//Generating axis`s
-	// 	const myYaxs = diag
-	// 		.append("g")
-	// 		.call(yAxis)
-	// 		.attr("id", "y-axis");
-	// 	const myXaxs = diag
-	// 		.append("g")
-	// 		.call(xAxis)
-	// 		.attr("id", "x-axis")
-	// 		.attr("transform", `translate(0,${d.h})`);
-	// 	/*----------Tooltip------------*/
-	// 	let tooltipDiv = d3
-	// 		.select("body")
-	// 		.append("div")
-	// 		.attr("id", "tooltip")
-	// 		.attr("class", "mytooltip")
-	// 		.attr("data-year", d.year)
-	// 		.style("opacity", 0);
-	// 	const tooltipParse = (obj) =>
-	// 		`${obj.year} - ${mon[obj.month - 1]}</br>${d3.format(".1f")(
-	// 			obj.variance + json.baseTemperature
-	// 		)}&deg;C</br>${d3.format(".1f")(obj.variance)}&deg;C`;
-	// 	/*----------Rectangles------------*/
-	// 	diag
-	// 		.selectAll("rect")
-	// 		.data(data)
-	// 		.enter()
-	// 		.append("rect")
-	// 		.attr("data-month", (d) => d.month - 1)
-	// 		.attr("data-year", (d) => d.year)
-	// 		.attr("data-temp", (d) => d.variance)
-	// 		.attr("class", "cell")
-	// 		.attr("x", (d) => x(d.year))
-	// 		.attr("y", (d) => y(d.month))
-	// 		.attr("height", b.h)
-	// 		.attr("width", b.w)
-	// 		.attr("fill", (d) => color(d.variance + json.baseTemperature))
-	// 		.on("mouseover", (d) => {
-	// 			tooltipDiv
-	// 				.attr("data-year", d.year)
-	// 				.transition()
-	// 				.duration(100)
-	// 				.style("opacity", 0.9);
-	// 			tooltipDiv
-	// 				.html(tooltipParse(d))
-	// 				.style("left", d3.event.pageX + 10 + "px")
-	// 				.style("top", d3.event.pageY - 80 + "px");
-	// 		})
-	// 		.on("mouseout", () => {
-	// 			tooltipDiv
-	// 				.transition()
-	// 				.duration(300)
-	// 				.style("opacity", 0);
-	// 		});
-	// 	/*----------Title + Description------------*/
-	// 	const title = svg
-	// 		.append("text")
-	// 		.attr("id", "title")
-	// 		.text("Monthly Global Land-Surface Temperature");
-	// 	const description = svg
-	// 		.append("text")
-	// 		.attr("id", "description")
-	// 		.html(
-	// 			`${d3.min(x.domain())}-${d3.max(x.domain())}, base temperature - ${
-	// 				json.baseTemperature
-	// 			}&deg;C`
-	// 		);
-	// 	//Centering title and description in data and top margin
-	// 	title
-	// 		.attr(
-	// 			"x",
-	// 			mg.l + d.w / 2 - title.node().getBoundingClientRect().width / 2
-	// 		)
-	// 		.attr("y", title.node().getBoundingClientRect().height / 2 + 20);
-	// 	description
-	// 		.attr(
-	// 			"x",
-	// 			mg.l + d.w / 2 - description.node().getBoundingClientRect().width / 2
-	// 		)
-	// 		.attr(
-	// 			"y",
-	// 			title.node().getBoundingClientRect().height +
-	// 				description.node().getBoundingClientRect().height +
-	// 				10
-	// 		);
-	// 	/*----------Legend------------*/
-	// 	const legend = svg.append("g").attr("id", "legend");
-	// 	legend
-	// 		.selectAll("rect")
-	// 		.data(clr)
-	// 		.enter()
-	// 		.append("rect")
-	// 		.attr("x", (d, i) => i * lg.w)
-	// 		.attr("height", lg.h)
-	// 		.attr("width", lg.w)
-	// 		.attr("fill", (d) => d)
-	// 		.attr("stroke", "black");
-	// 	//Legend scale
-	// 	const lgSc = d3
-	// 		.scaleLinear()
-	// 		.domain(color.domain())
-	// 		.range([0, lg.w * clr.length]);
-	// 	const tickVals = color.range().reduce((acc, d, i) => {
-	// 		let tmp = color.invertExtent(d);
-	// 		acc.push(tmp[0]);
-	// 		if (i === color.range().length - 1) acc.push(tmp[1]);
-	// 		return acc;
-	// 	}, []);
-	// 	const lgAxis = d3
-	// 		.axisBottom(lgSc)
-	// 		.tickFormat(d3.format(".1f"))
-	// 		.tickValues(tickVals);
-	// 	legend
-	// 		.append("g")
-	// 		.call(lgAxis)
-	// 		.attr("id", "lg-axis")
-	// 		.attr("transform", `translate(0,${lg.h})`);
-	// 	//Centering legend in lower margin
-	// 	const tmp = myXaxs.node().getBoundingClientRect().height;
-	// 	legend.attr(
-	// 		"transform",
-	// 		`translate(${mg.l +
-	// 			(d.w - legend.node().getBoundingClientRect().width) / 2},${mg.t +
-	// 			d.h +
-	// 			tmp +
-	// 			(mg.b - tmp) / 2 -
-	// 			legend.node().getBoundingClientRect().height / 2})`
-	// 	);
-	// };
 });
